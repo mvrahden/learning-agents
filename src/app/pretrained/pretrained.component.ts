497b2162e8d8a6146c25ffe5e13713272a640ffb
Menno <menno.vrahden@gmail.com>
2018-04-01 20:22:07 +0200

    add pretrained brain loading

--- src/app/pretrained/pretrained.component.ts
+++ src/app/pretrained/pretrained.component.ts
@@ -4,18 +4,96 @@ import { World } from 'learning-agents-model';
 
 import { ViewControl } from '../viewcontrol/viewcontrol';
 
+declare let $: any;
+
 @Component({
   selector: 'app-pretrained',
   templateUrl: './pretrained.component.html',
   styleUrls: ['./pretrained.component.css']
 })
 export class PretrainedComponent extends ViewControl {
+  // "https://raw.githubusercontent.com/mvrahden/learning-agents/master/src/app/pretrained/brains/agent-brain-state.0.json"
+  readonly baseUrl: string = "https://raw.githubusercontent.com/mvrahden/learning-agents"
+                              + "/master/src/app/pretrained/brains/agent-brain-state.";
+  loadedBrainCounter: number = 0;
+  isFullyLoaded = false;
+
   constructor() {
     super();
   }
 
   protected initConcrete(): void {
-    // LOAD BRAIN
+    this.loadedBrainCounter = 0;
+    this.isFullyLoaded = false;
+    this.switchAgentsToEvaluationMode();
+    this.loadBrains();
+  }
+
+  private switchAgentsToEvaluationMode(): void {
+    this.world.switchTrainingModeOfAgents(false);
+  }
+
+  private reload(): void {
+    this.initConcrete();
+  }
+
+  private async loadBrains(): Promise<any> {
+    for (let i = 0; i < this.maxAgents; i++) {
+      const brain = this.queryBrain(i);
+      const agent = this.world.agents[i];
+    }
+    return null;
+  }
+
+  private async queryBrain(id: number): Promise<any> {
+    const request = this.getHttpRequestFor(id);
+    
+    const self = this;
+
+    const promise = new Promise((resolve, reject) => {
+      $.getJSON(request).done((data) => {
+        self.resolve(id, data);
+      }).fail((jqxhr, textStatus, error) => {
+        self.reject(id, jqxhr, textStatus, error);
+      });
+    })
+
+    return promise;
+  }
+
+  private resolve(id: number, data: string): void {
+    this.loadBrain(id, data);
+    console.log("Brain of Agent " + (id + 1) + " loaded successfully!");
+    this.registerLoading(id);
+  }
+
+  private loadBrain(id: number, brain: string): void {
+    this.world.agents[id].load(brain);
+  }
+
+  private registerLoading(id: number): void {
+    this.loadedBrainCounter++;
+    if(this.loadedBrainCounter === this.world.agents.length) {
+      this.isFullyLoaded = true;
+    }
+  }
+
+  private reject(id: number, jqxhr: any, textStatus: string, error: string): void {
+    console.log("Brain of Agent " + (id + 1) + " didn't load successfully!");
+    console.log(textStatus + ", " + error);
+  }
+
+  private getHttpRequestFor(id: number): any {
+    return {
+      type: 'GET',
+      dataType: 'json',
+      contentType: 'application/json;charset=utf-8',
+      url: this.getUrlFor(id)
+    };
+  }
+
+  private getUrlFor(id: number): string {
+    return this.baseUrl + id + ".json";
   }
 
   protected handleEvaluationPeriodActivation(): void { }
