719a799c19d0276e134739f5f8838fc46d8e79fc
Menno <menno.vrahden@gmail.com>
2018-04-01 20:27:07 +0200

    change material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { MatTabsModule } from '@angular/material/tabs';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
@@ -21,8 +20,7 @@ import { DQNComponent } from './dqn-method/dqn-method.component';
   imports: [
     BrowserModule,
     FormsModule,
-    MatTabsModule,
-    BrowserAnimationsModule,
+    MatToolbarModule,
     MatCardModule,
     MatButtonModule,
     MatButtonToggleModule,
